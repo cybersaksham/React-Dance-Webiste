e1bd47e169bd1f9523b398159e927e32c7486341
Saksham <2020UCP1801@mnit.ac.in>
2021-09-16 09:50:54 +0530

    navbar component

---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../Style/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
