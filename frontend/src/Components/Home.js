5f35ad8a9f89577750c37dfe1ba2a413c80dc459
Saksham <2020UCP1801@mnit.ac.in>
2021-09-16 15:36:25 +0530

    services section

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "../Style/Home.css";
+import left from "../Gallery/left.png";
+import mid from "../Gallery/mid.png";
+import right from "../Gallery/right.png";
 
 const Home = () => {
   return (
@@ -8,7 +11,44 @@ const Home = () => {
         <h1>Dance Website</h1>
         <p>This is a dance website made in react using pure css</p>
       </section>
-      <section id="serviceSection"></section>
+      <section id="serviceSection">
+        <h3>Our Services</h3>
+        <div id="serviceCards">
+          <div className="card">
+            <h5>Hard Work</h5>
+            <div className="card-content">
+              <img src={left} alt="..." />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+                fuga iure repudiandae quasi rerum quam eius culpa assumenda
+                expedita magni!
+              </p>
+            </div>
+          </div>
+          <div className="card">
+            <h5>Patience</h5>
+            <div className="card-content">
+              <img src={mid} alt="..." />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+                fuga iure repudiandae quasi rerum quam eius culpa assumenda
+                expedita magni!
+              </p>
+            </div>
+          </div>
+          <div className="card">
+            <h5>Perseverence</h5>
+            <div className="card-content">
+              <img src={right} alt="..." />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+                fuga iure repudiandae quasi rerum quam eius culpa assumenda
+                expedita magni!
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
       <section id="sponserSection"></section>
     </div>
   );
