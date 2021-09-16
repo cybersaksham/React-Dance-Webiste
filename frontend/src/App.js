49391b06446640856200af0c5d075dd74f36085d
Saksham <2020UCP1801@mnit.ac.in>
2021-09-16 14:25:04 +0530

    about section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,23 @@ import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Contact from "./Components/Contact";
 import ContactState from "./Context/Contact/ContactState";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <ContactState>
       <Router>
         <Navbar />
-        <Switch>
-          <Route exact path="/">
-            {/* <Home /> */}
-          </Route>
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
-        </Switch>
+        <div id="mainBody">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </div>
       </Router>
     </ContactState>
   );
---
+++ frontend/src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../Style/Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <section id="aboutSection">
+        <p>Dance Website</p>
+        <p>This is a dance website made in react using pure css</p>
+      </section>
+      <section id="serviceSection"></section>
+      <section id="sponserSection"></section>
+    </div>
+  );
+};
+
+export default Home;
