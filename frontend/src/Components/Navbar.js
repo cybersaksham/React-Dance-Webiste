0fb45f0dee992d10737594fdb9464c2ac72c33b1
Saksham <2020UCP1801@mnit.ac.in>
2021-09-16 15:41:39 +0530

    navbar logo

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../Style/Navbar.css";
+import logo from "../Gallery/logo.png";
 
 const Navbar = () => {
   const location = useLocation();
 
   return (
     <nav>
+      <img src={logo} alt="..." />
       <ul>
         <li className={location.pathname === "/" ? "activeTabs" : ""}>
           <Link to="/">Home</Link>
