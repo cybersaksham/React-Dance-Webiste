cbe0257eb2842907a4586675b05a08a43193f67b
Saksham <2020UCP1801@mnit.ac.in>
2021-09-16 09:28:20 +0530

    contact post request

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const connectToMongo = require("./db");
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const cors = require("cors");
 
 // Connecting to database
@@ -14,6 +15,28 @@ app.use(cors());
 
 // To use json bundles
 app.use(express.json());
+
+// Routes
+app.post(
+  "/contact",
+  [
+    // Validators
+    body("name", "Name must be 3 characters long.").isLength({ min: 3 }),
+    body("email", "Enter correct email").isEmail(),
+    body("message", "Message must be 20 characters long.").isLength({
+      min: 20,
+    }),
+  ],
+  async (req, res) => {
+    // Sending error if validator failed
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array()[0].msg });
+    }
+
+    res.send(req.body);
+  }
+);
 
 // Listening
 app.listen(port, () => {
